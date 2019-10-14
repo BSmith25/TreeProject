cc0fdd26f05682d118e670064623c47772211591
Brianne25 <briannekellysmith@gmail.com>
2019-10-14 08:11:40 -0400

    cleaning up code

--- ProjectFiles/Data_Organization.sh
+++ ProjectFiles/Data_Organization.sh
@@ -3,20 +3,21 @@
 # Note that wget is needed
 
 # First create the data directories:
-mkdir -p /Data;
-	mkdir -p /Data/311;
-	mkdir -p /Data/FinalOutputs;
-	mkdir -p /Data/ModelData;
-	mkdir -p /Data/spatialdatasets;
-	mkdir -p /Data/Trees;
+mkdir Data;
+	mkdir Data/311;
+	mkdir Data/FinalOutputs;
+	mkdir Data/ModelData;
+	mkdir Data/spatialdatasets;
+	mkdir Data/Trees;
 
 # Now move on to the 311 data,
-# downlaod the NYC 311 service requests from NYC Open Data into the proper folder
-wget https://nycopendata.socrata.com/api/views/erm2-nwe9/rows.csv?accessType=DOWNLOAD -O /Data/311/311_Service_Requests_from_2010_to_Present.csv
+# downlaod all NYC 311 service requests from NYC Open Data into the proper folder
+wget https://nycopendata.socrata.com/api/views/erm2-nwe9/rows.csv?accessType=DOWNLOAD -O Data/311/311_Service_Requests_from_2010_to_Present.csv
 
 	# Get all fallen trees/branches service requests and drop un-needed columns
-	grep Unique 311_Service_Requests_from_2010_to_Present.csv > temp
-	grep Fallen 311_Service_Requests_from_2010_to_Present.csv >> temp
+	# Note we will need to grep a bunch of columns based on a keyword (Fallen) and then use other keywords (mostly agency names) to remove un-wanted data
+	grep Unique Data/311/311_Service_Requests_from_2010_to_Present.csv > temp
+	grep Fallen Data/311/311_Service_Requests_from_2010_to_Present.csv >> temp
 	sed -i '/DSNY/d' temp
 	sed -i '/DCA/d' temp
 	sed -i '/DEP/d' temp
@@ -24,11 +25,10 @@ wget https://nycopendata.socrata.com/api/views/erm2-nwe9/rows.csv?accessType=DOW
 	sed -i '/DOITT/d' temp
 	sed -i '/DOB/d' temp
 	sed -i '/ACS/d' temp
-	sed -i '/Entire/d' temp
-	sed -i '/Park/d' temp
-	cut -d"," -f 1-4,6-15,20,26-28,39-40 temp > Tree_Fall_311.csv
+	cut -d"," -f 1-4,6-15,20,26-28,39-40 temp > Data/311/Tree_Fall_311.csv
 
 	# Get all damaged trees/branches service requests and drop un-needed columns
+	# Note we will need to grep a bunch of columns based on several keywords and then use other keywords (mostly agency names) to remove un-wanted data
 	grep Unique 311_Service_Requests_from_2010_to_Present.csv > temp
 	grep Dead 311_Service_Requests_from_2010_to_Present.csv >> temp
 	grep Damaged 311_Service_Requests_from_2010_to_Present.csv >> temp
@@ -44,8 +44,7 @@ wget https://nycopendata.socrata.com/api/views/erm2-nwe9/rows.csv?accessType=DOW
 	sed -i '/DOB/d' temp
 	sed -i '/ACS/d' temp
 	sed -i '/Animal/d' temp
-	sed -i '/Park/d' temp
-	cut -d"," -f 1-4,6-15,20,26-28,39-40 temp > Tree_Damage_311.csv
+	cut -d"," -f 1-4,6-15,20,26-28,39-40 temp > Data/311/Tree_Service_Requests_311.csv
 
 	# Get all overhanging trees/branches service requests and drop un-needed columns
 	grep Unique 311_Service_Requests_from_2010_to_Present.csv > temp
@@ -58,20 +57,22 @@ wget https://nycopendata.socrata.com/api/views/erm2-nwe9/rows.csv?accessType=DOW
 	sed -i '/DOB/d' temp
 	sed -i '/ACS/d' temp
 	sed -i '/Animal/d' temp
-	sed -i '/Park/d' temp
-	cut -d"," -f 1-4,6-15,20,26-28,39-40 temp >> Tree_Damage_311.csv
+	cut -d"," -f 1-4,6-15,20,26-28,39-40 temp >> Data/311/Tree_Service_Requests_311.csv
+	
+	# delete the temp file
+	rm temp
 
 # Next, work on the tree census data
 	# Downlaod the data from NYC Open Data and put it in the Data/Trees folder
-	wget https://data.cityofnewyork.us/api/geospatial/pi5s-9p35?method=export&format=Shapefile -O /Data/Trees/temp.zip && unzip temp.zip -d /Data/Trees/ &&	rm temp.zip
+	wget https://data.cityofnewyork.us/api/geospatial/pi5s-9p35?method=export&format=Shapefile -O temp.zip && unzip temp.zip -d /Data/Trees/ &&	rm temp.zip
 	
 # Finally, download the spatial datasets from github to Data/spatialdatasets and unzip them or rename them as needed
 # Note these datasets were generated from open data and merged in QGIS
 	# First the building height and impervious % dataset
-	wget https://github.com/BSmith25/TreeProject/blob/master/ProjectFiles/data_with_imp_bldghght.zip -O /Data/spatialdatasets/temp.zip && unzip temp.zip -d /Data/spatialdatasets/ &&	rm temp.zip
+	wget https://github.com/BSmith25/TreeProject/blob/master/ProjectFiles/data_with_imp_bldghght.zip -O temp.zip && unzip temp.zip -d /Data/spatialdatasets/ &&	rm temp.zip
 	mv  /Data/spatialdatasets/data_with_imp_bldghght_for_github.csv /Data/spatialdatasets/data_with_imp_bldghght.csv
 
 	# Next the service requests per fall in all the zipcodes
-	wget https://github.com/BSmith25/TreeProject/blob/master/ProjectFiles/Fall_Count.zip -O /Data/spatialdatasets/temp.zip && unzip temp.zip -d /Data/spatialdatasets/ &&	rm temp.zip
+	wget https://github.com/BSmith25/TreeProject/blob/master/ProjectFiles/Fall_Count.zip -O temp.zip && unzip temp.zip -d /Data/spatialdatasets/ &&	rm temp.zip
 
 	
